e2ee85a06ca4922d07dd303dcb5c8d52a748554a
Aaron Moraes <147651798+AaronMoraes@users.noreply.github.com>
2026-08-19 14:43:26 +0000

    feat: adiciona validacoes ao pedido DTO

--- ApiEstudos/Dtos/PedidoDto.cs
+++ ApiEstudos/Dtos/PedidoDto.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ApiEstudos.Dtos;
 
 public class PedidoDto
 {
+    [Required]
     public string Cliente {get; set;} = string.Empty;
+
+    [Range(0.01, double.MaxValue)]
     public decimal Valor {get; set;}
+
+    [Required]
+    [RegularExpression("^(Pendente|Enviado|Cancelado)$^")]
     public string Status {get; set;} = string.Empty;
 }
